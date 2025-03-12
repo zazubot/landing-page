1b646a550befea222191717ac15939fe0ccc8a85
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-03-13 01:54:30 +0200

    change imsges

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import PageIllustration from "@/components/page-illustration";
-import Avatar01 from "@/public/images/avatar-01.jpg";
-import Avatar02 from "@/public/images/avatar-02.jpg";
-import Avatar03 from "@/public/images/avatar-03.jpg";
-import Avatar04 from "@/public/images/avatar-04.jpg";
-import Avatar05 from "@/public/images/avatar-05.jpg";
-import Avatar06 from "@/public/images/avatar-06.jpg";
+import Avatar01 from "@/public/images/ai/ElevenLabs.jpeg";
+import Avatar02 from "@/public/images/ai/mistral.jpg";
+import Avatar03 from "@/public/images/ai/sheet.png";
+import Avatar04 from "@/public/images/ai/openai.jpg";
+import Avatar05 from "@/public/images/ai/zendesk.png";
+import Avatar06 from "@/public/images/ai/zapier.png";
 
 export default function HeroHome() {
   return (
@@ -20,47 +20,47 @@ export default function HeroHome() {
               className="mb-6 border-y [border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1]"
               data-aos="zoom-y-out"
             >
-              <div className="-mx-0.5 flex justify-center -space-x-3">
+              <div className="-mx-0.5 flex justify-center space-x-3">
                 <Image
                   className="box-content rounded-full border-2 border-neutral-50"
                   src={Avatar01}
-                  width={32}
-                  height={32}
+                  width={64}
+                  height={64}
                   alt="Avatar 01"
                 />
                 <Image
                   className="box-content rounded-full border-2 border-neutral-50"
                   src={Avatar02}
-                  width={32}
-                  height={32}
+                  width={64}
+                  height={64}
                   alt="Avatar 01"
                 />
                 <Image
                   className="box-content rounded-full border-2 border-neutral-50"
                   src={Avatar03}
-                  width={32}
-                  height={32}
+                  width={64}
+                  height={64}
                   alt="Avatar 02"
                 />
                 <Image
                   className="box-content rounded-full border-2 border-neutral-50"
                   src={Avatar04}
-                  width={32}
-                  height={32}
+                  width={64}
+                  height={64}
                   alt="Avatar 03"
                 />
                 <Image
-                  className="box-content rounded-full border-2 border-neutral-50"
+                  className="box-content rounded-full   border-2 border-neutral-50"
                   src={Avatar05}
-                  width={32}
-                  height={32}
+                  width={64}
+                  height={64}
                   alt="Avatar 04"
                 />
                 <Image
                   className="box-content rounded-full border-2 border-neutral-50"
                   src={Avatar06}
-                  width={32}
-                  height={32}
+                  width={64}
+                  height={64}
                   alt="Avatar 05"
                 />
               </div>
