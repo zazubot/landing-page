e5553b29885ee48e53281ebc6894cc19269090fc
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-08-09 18:48:17 +0300

    handleSubmit

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -7,10 +7,41 @@ import Avatar03 from "@/public/images/ai/sheet.png";
 import Avatar04 from "@/public/images/ai/openai.jpg";
 import Avatar05 from "@/public/images/ai/zendesk.png";
 import Avatar06 from "@/public/images/ai/cal.png";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function HeroHome() {
   const [showModal, setShowModal] = useState(false);
+  const form = useRef<HTMLFormElement>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // Function to handle form submission
+  const handleSubmit = (
+    e: React.FormEvent<HTMLFormElement | MouseEvent | HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    setIsLoading(true);
+    if (form.current) {
+      // Use emailjs to send the form data
+      emailjs
+        .sendForm(
+          "service_762lcihd",
+          "template_q6n4qwu",
+          form.current,
+          "CqLOSwRxQRCVbGlNk"
+        )
+        .then(() => {
+          alert("thanks for your interest, we will contact you soon");
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error("Email sending error:", error);
+          alert("Failed to send email. Please try again later.");
+        })
+        .finally(() => setIsLoading(false));
+    }
+  };
+
+  // Function to handle modal close
 
   return (
     <section className="relative">
@@ -71,11 +102,10 @@ export default function HeroHome() {
             <h1
               className="mb-6 border-y text-5xl font-bold [border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1] md:text-6xl"
               data-aos="zoom-y-out"
-              data-aos-delay={150}
             >
-              Hack the bot game with Agents
-              <br className="max-lg:hidden" />
-              Build faster, Chat smarter
+              <span data-aos-delay={150}> Hack the bot game</span>
+              <br className="max-lg:hidden " />
+              <span className="text-green-800">Build faster, Chat smarter</span>
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -116,16 +146,33 @@ export default function HeroHome() {
             </div>
             {/* {add inter your bussiness email and signup button } */}{" "}
             <div className="mt-8">
-              <form className="flex flex-col items-center justify-center sm:flex-row">
+              <form
+                ref={form}
+                onSubmit={handleSubmit}
+                className="flex flex-col items-center justify-center sm:flex-row"
+              >
                 {/* {make the input much bigger} */}
                 <input
+                  type="text"
+                  required
+                  name="name"
+                  defaultValue="ZazuBot"
+                  id="name"
+                  hidden
+                />
+                <input
                   type="email"
+                  required
+                  name="email"
+                  id="email"
                   placeholder="Enter your business email"
                   className="w-full max-w-xl appearance-none rounded border-2 border-neutral-300 bg-white   text-2xl text-neutral-800 placeholder-neutral-400 shadow-sm focus:border-green-500 focus:outline-none focus:ring-4 focus:ring-green-200"
                   // style={{ minHeight: "4rem" }}
                 />
                 <button
                   type="submit"
+                  disabled={isLoading}
+                  onClick={(e) => handleSubmit(e)}
                   className="btn mt-4 w-full bg-green-800 text-neutral-200 shadow-sm hover:bg-green-900 sm:ml-4 sm:mt-0 sm:w-auto"
                 >
                   Sign Up Free
