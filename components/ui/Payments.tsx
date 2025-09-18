dff6fb456d339d883dff39248d294c14697e48d1
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-09-18 16:06:45 +0300

    flow-1

--- components/ui/Payments.tsx
+++ components/ui/Payments.tsx
@@ -5,6 +5,9 @@ import Mada from "@/public/images/providers/Mada.svg";
 import visa from "@/public/images/providers/Visa.png";
 import master from "@/public/images/providers/Master.png";
 
+import citc_logo from "@/public/images/providers/citc_logo.png";
+import metalogo from "@/public/images/providers/metalogo.png";
+
 export default function Payments() {
   return (
     <div className="pb-12 text-center md:pb-16">
@@ -12,6 +15,20 @@ export default function Payments() {
         className="mb-6 border-y [border-image:linear-gradient(to_right,transparent,--theme(--color-slate-300/.8),transparent)1]"
         data-aos="zoom-y-out"
       >
+        <div className="-mx-0.5 flex justify-center space-3 p-8">
+          <Image
+            className="box-content"
+            src={citc_logo}
+            height={80}
+            alt="citc_logo 01"
+          />
+          <Image
+            className="box-content  "
+            src={metalogo}
+            height={80}
+            alt="metalogo 01"
+          />
+        </div>
         <div className="-mx-0.5 flex justify-center space-x-3">
           <Image
             className="box-content rounded-full border-2 border-neutral-50"
